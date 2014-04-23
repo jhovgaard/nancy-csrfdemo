0a20064b3b3f994aa2d0d77c7e8ef43472a5660a
Jonas Hovgaard <j@jhovgaard.dk>
2014-04-23 02:32:06 +0200

    Initial

---
+++ NancyCsrfDemo/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace NancyCsrfDemo
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            Nancy.Security.Csrf.Enable(pipelines);
+        }
+    }
+}
---
+++ NancyCsrfDemo/HomeModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using Nancy;
+using Nancy.Security;
+
+namespace NancyCsrfDemo
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = p =>
+            {
+                return View["Index"];
+            };
+
+            Post["/"] = p =>
+            {
+                try
+                {
+                    this.ValidateCsrfToken();
+                }
+                catch (CsrfValidationException)
+                {
+                    return Response.AsText("Csrf Token not valid.").WithStatusCode(403);
+                }
+                
+                ViewBag.Name = Request.Form.Name;
+                return View["Index"];
+            };
+
+        }
+    }
+}   
